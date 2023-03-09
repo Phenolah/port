31a312e08d135fe555a2f63ea61c7f9bea0419cd
Phenolah <104977409+Phenolah@users.noreply.github.com>
2023-03-09 01:05:52 -0800

    Update settings.py

--- PortfolioApp/settings.py
+++ PortfolioApp/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIR =(
+STATICFILES_DIRS =(
     os.path.join(BASE_DIR, 'static/')
 )
 MEDIA_URL ='/images/'
