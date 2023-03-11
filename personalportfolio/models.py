57406d041896ecea624ff5fd720a792a91adfebf
Phenolah <104977409+Phenolah@users.noreply.github.com>
2023-03-11 00:25:15 -0800

    Update models.py

--- personalportfolio/models.py
+++ personalportfolio/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Skills(models.Model):
+    class Meta:
+        verbose_name_plural = "skills"
+        verbose_name = 'skill'
+
+    name = models.CharField(max_length=50, blank=True, null=True)
+    description = models.TextField()
+    image = models.FileField(blank=True, null=True, upload_to="images")
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Contact(models.Model):
+    class Meta:
+        verbose_name_plural = "contacts"
+        verbose_name = "contact"
+    name = models.CharField(max_length=50, blank=True, null=True)
+    email = models.EmailField(null=True, blank=True)
+    subject = models.CharField(max_length=100, blan=True, null=True )
+    message = models.TextField()
+
+    def __str__(self):
+        return f"{self.subject}"
