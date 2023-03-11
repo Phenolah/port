84e2ce405a7d24160656fc275308d320adbd4a97
Phenolah <104977409+Phenolah@users.noreply.github.com>
2023-03-11 00:21:53 -0800

    Update settings.py

--- PortfolioApp/settings.py
+++ PortfolioApp/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'PortfolioApp.context_processors.portfolio_context',
             ],
         },
     },
