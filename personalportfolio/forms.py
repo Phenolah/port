1bc5d03efe980e084abf0cab364ea2ee358ccd3c
Phenolah <104977409+Phenolah@users.noreply.github.com>
2023-03-11 00:24:19 -0800

    Update forms.py

---
+++ personalportfolio/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+
+class ContactForm(forms.Form):
+    name = forms.CharField( widget=forms.TextInput(attrs={
+        'placeholder': 'Your name',
+        'type': 'text',
+        'name': 'name',
+        'class': 'form-control',
+    }))
+    email= forms.EmailField(widget=forms.TextInput(attrs={
+        'placeholder': 'Enter your Email',
+        'type': 'text',
+        'name': 'email',
+        'class': 'form-control',
+    }))
+    subject= forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Subject matter',
+        'type': 'text',
+        'name': 'subject',
+        'class': 'form-control',
+    }))
+    message=forms.TextInput()
