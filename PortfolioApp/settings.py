88e301fde2db200be1e7b0119f46ec4831020a6c
Phenolah <104977409+Phenolah@users.noreply.github.com>
2023-03-12 06:50:19 -0700

    Update settings.py

--- PortfolioApp/settings.py
+++ PortfolioApp/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'personalportfolio',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
