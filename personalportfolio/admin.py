55be4672c392a34e9fc1df125c04433e3043b7a5
Phenolah <104977409+Phenolah@users.noreply.github.com>
2023-03-14 06:50:27 -0700

    Update admin.py

--- personalportfolio/admin.py
+++ personalportfolio/admin.py
@@ -3,4 +3,15 @@ from .models import *
 # Register your models here.
 
 admin.site.register(Contact)
+
 admin.site.register(Skills)
+
+admin.site.register(PortfolioProjects)
+#class PortfolioProjectAdmin(admin.ModelAdmin):
+    #readonly_fields = ('slug')
+
+admin.site.register(PortfolioProfile)
+
+admin.site.register(Home)
+
+admin.site.register(About)
