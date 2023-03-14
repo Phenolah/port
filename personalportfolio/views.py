88a11fe2fdab9195e4880b6175fec2d8b9a1469d
Phenolah <104977409+Phenolah@users.noreply.github.com>
2023-03-14 06:53:06 -0700

    Update views.py

--- personalportfolio/views.py
+++ personalportfolio/views.py
@@ -1,10 +1,41 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from .forms import *
+from .models import *
+from django.views.generic.list import ListView
+from django.core.mail import send_mail, BadHeaderError
 # Create your views here.
 
-def home(request):
-    form = ContactForm()
-    context = {
-        'form': form,
-    }
-    return render(request, 'home.html', context)
+class HomeView(ListView):
+    context_object_name = 'skills'
+    def get(self, *args, **kwargs):
+        form = ContactForm()
+        context = {
+            'form': form,
+        }
+        return render(self.request, 'home.html', context)
+    def get(self, *args, **kwargs):
+        form = ContactForm()
+        context={
+            'form': form
+        }
+        if self.request.method == "POST":
+            form = ContactForm(self.request.POST)
+            if form.is_valid():
+                name = form.cleaned_data['name']
+                subject = form.cleaned_data['subject']
+                message = form.cleaned_data['message']
+                sender = form.cleaned_data['email']
+                recipients = ['phenolaha@gmail.com']
+                try:
+                    send_mail(name, subject, message, sender, recipients)
+                    form.save()
+                except BadHeaderError:
+                    return HttpResponse("Invalid header found ")
+                return redirect('success')
+                context = {'success': True}
+        return render(self.request, 'home.html', context)
+
+def successView(request):
+    return HttpResponse (request, "Success! Thank your for your message. I will get back to you as soon as possible:)")
+
+
