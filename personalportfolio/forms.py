49e01cc136416918bd063908d309009240e65b28
Phenolah <104977409+Phenolah@users.noreply.github.com>
2023-03-14 06:55:31 -0700

    Update forms.py

--- personalportfolio/forms.py
+++ personalportfolio/forms.py
@@ -1,27 +1,35 @@
 from django import forms
+from .models import *
 
-class ContactForm(forms.Form):
-    name = forms.CharField( widget=forms.TextInput(attrs={
+class ContactForm(forms.ModelForm):
+    name = forms.CharField( required=True, max_length=100,
+                widget=forms.TextInput(attrs={
         'placeholder': 'Your name',
         'type': 'text',
         'name': 'name',
         'class': 'form-control',
     }))
-    email= forms.EmailField(widget=forms.TextInput(attrs={
+    email= forms.EmailField( required=True,
+            widget=forms.TextInput(attrs={
         'placeholder': 'Enter your Email',
         'type': 'text',
         'name': 'email',
         'class': 'form-control',
     }))
-    subject= forms.CharField(widget=forms.TextInput(attrs={
+    subject= forms.CharField( max_length=100,
+                widget=forms.TextInput(attrs={
         'placeholder': 'Subject matter',
         'type': 'text',
         'name': 'subject',
         'class': 'form-control',
     }))
-    message=forms.CharField(widget=forms.TextInput(attrs={
+    message=forms.CharField( required=True,
+        widget=forms.Textarea(attrs={
         'placeholder': 'Enter your message',
         'type': 'text',
         'name': 'messages',
-        'class': 'form-control',
+        'rows': 7,
     }))
+    class Meta:
+        model = PortfolioProfile
+        fields = ('name', 'email', 'subject', 'message')
