71be6bd810cccb8ac7df8f1bd64a0fd922fae2dd
Phenolah <104977409+Phenolah@users.noreply.github.com>
2023-03-20 12:27:52 -0700

    Add files via upload

---
+++ static/js/index.js
@@ -0,0 +1,16 @@
+.body{
+  background: #0d0d0d;
+  padding-bottom: 6.5rem;
+}
+
+.navbar{
+ position: fixed;
+ bottom:0; left:0; right:0;
+ display: flex;
+ z-index: 1000;
+ align-items: right;
+}
+.navbar .a{
+   color:#fff;
+
+}
