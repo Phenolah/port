26112cd4e0856ab7be8a19532fc77ca6056c528c
Phenolah <104977409+Phenolah@users.noreply.github.com>
2023-03-23 06:45:35 -0700

    Update admin.py

--- personalportfolio/admin.py
+++ personalportfolio/admin.py
@@ -1,4 +1,3 @@
-from django.contrib import admin
 from .models import *
 # Register your models here.
 
@@ -15,3 +14,7 @@ admin.site.register(PortfolioProfile)
 admin.site.register(Home)
 
 admin.site.register(About)
+
+admin.site.register(Blog)
+
+admin.site.register(Certificate)
