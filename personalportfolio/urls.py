b352a527a8335c28163cf9b592559baef9ee2a18
Phenolah <104977409+Phenolah@users.noreply.github.com>
2023-03-28 12:20:29 -0700

    Update urls.py

--- personalportfolio/urls.py
+++ personalportfolio/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='homepage'),
     path('about/', views.about, name='about'),
-    path('skills/', views.skills, name='skills'),
+    path('skills/', views.SkillsView.as_view(), name='skills'),
     path('portfolio/', views.PortfolioView.as_view(), name='portfolio'),
     path('portfolio/', views.PortfolioDetailView.as_view(), name='portfolio'),
     path('contact/', views.ContactView.as_view(), name='contact'),
