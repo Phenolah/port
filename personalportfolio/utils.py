d401ea2771f33c6f6cc1951051f4594cf8a6d000
Phenolah <104977409+Phenolah@users.noreply.github.com>
2023-04-20 13:35:57 -0700

    Create utils.py

---
+++ personalportfolio/utils.py
@@ -0,0 +1,21 @@
+from django.contrib.gis.geoip2 import GeoIP2
+# Helper Functions
+
+def get_geo(ip):
+    g = GeoIP2()
+    country = g.country(ip)
+    city = g.city(ip)
+    lat, long = g.lat_lon(ip)
+    return country, city, lat, long
+def get_center_coordinates(latA, longA, latB=None, longB=None):
+    cord = (latA, longA)
+    if latB:
+        cord = [(latA+latB)/2, (longA+longB)/2]
+    return cord
+def get_zoom(distance):
+    if distance <= 100:
+        return 8
+    elif distance > 100 and distance <= 5000:
+        return 4
+    else:
+        return 2
