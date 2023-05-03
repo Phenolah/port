a7f56371f6d2a71c857506feba76d9969e626da0
Phenolah <104977409+Phenolah@users.noreply.github.com>
2023-05-03 08:41:56 -0700

    Update views.py

--- personalportfolio/views.py
+++ personalportfolio/views.py
@@ -57,7 +57,8 @@ class HomeView(generic.TemplateView):
             # Redirect the user to the success page
             return super().form_valid(contact_form)
         elif cv_form.is_valid():
-            cv_form.save()
+            cv_upload = CvFileForm(request.FILES['resume'])
+            cv_upload.save()
             return redirect("homepage")
         else:
             context = self.get_context_data(**kwargs)
@@ -78,36 +79,6 @@ class HomeView(generic.TemplateView):
         messages.success(self.request, "Success! Thank you for contacting me. I'll get back to you as soon as possible")
         # Redirect the user to the success page
         return super().form_valid(form)
-
-    def route(self, request):
-        context = {"google_api_key": settings.GOOGLE_API_KEY}
-        return render(request, 'main/route.html', context)
-
-    def map(request):
-
-        lat_a = request.GET.get("lat_a")
-        long_a = request.GET.get("long_a")
-        lat_b = request.GET.get("lat_b")
-        long_b = request.GET.get("long_b")
-        directions = Directions(
-            lat_a=lat_a,
-            long_a=long_a,
-            lat_b=lat_b,
-            long_b=long_b
-        )
-
-        context = {
-            "google_api_key": settings.GOOGLE_API_KEY,
-            "lat_a": lat_a,
-            "long_a": long_a,
-            "lat_b": lat_b,
-            "long_b": long_b,
-            "origin": f'{lat_a}, {long_a}',
-            "destination": f'{lat_b}, {long_b}',
-            "directions": directions,
-
-        }
-        return render(request, 'main/map.html', context)
 
 
 class AboutView(generic.ListView):
@@ -208,6 +179,14 @@ class ContactView(generic.FormView):
     model = Contact
     form_class = ContactForm
 
+    def get_ip_address(request):
+        x_forwarded_fro = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_fro:
+            ip = x_forwarded_fro.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         distance = None
@@ -216,7 +195,7 @@ class ContactView(generic.FormView):
         measurement_form = MeasurementModelForm(self.request.POST or None)
         geolocator = Nominatim(user_agent="measurements")
 
-        ip_ = get_ip_address()
+        ip_ = get_ip_address(self.request)
         print(ip_)
         ip = '72.14.207.99'
         country, city, lat, lon = get_geo(ip)
@@ -227,9 +206,9 @@ class ContactView(generic.FormView):
         pointA = (l_lat, l_lon)
 
         # initial folium map
-        m = folium.Map(width=800, height=500, location=get_center_coordinates(l_lat,l_lon), zoom_start=100)
+        m = folium.Map(width=800, height=500, location=get_center_coordinates(l_lat, l_lon), zoom_start=100)
         # location marker
-        folium.Marker([l_lat, l_lon],tooltip='clock here for more', popup=city['city'],
+        folium.Marker([l_lat, l_lon], tooltip='clock here for more', popup=city['city'],
                       icon=folium.Icon(color="pink")).add_to(m)
 
         if measurement_form.is_valid():
@@ -245,18 +224,19 @@ class ContactView(generic.FormView):
             d_lon = destination.longitude
             pointB = (d_lat, d_lon)
 
-            #distance calculation
+            # distance calculation
             distance = round(geodesic(pointA, pointB).km, 2)
             # initial folium map
-            m = folium.Map(width=800, height=500, location=get_center_coordinates(l_lat, l_lon), zoom_start=get_zoom(distance))
+            m = folium.Map(width=800, height=500, location=get_center_coordinates(l_lat, l_lon),
+                           zoom_start=get_zoom(distance))
             # location marker
             folium.Marker([d_lat, d_lon], tooltip='clock here for more', popup=destination,
-                  icon = folium.Icon(color="pink")).add_to(m)
+                          icon=folium.Icon(color="pink")).add_to(m)
             # location marker
             folium.Marker([l_lat, l_lon], tooltip='clock here for more', popup=city['city'],
                           icon=folium.Icon(color="red", icon='cloud')).add_to(m)
 
-            #draw the line between location and destination
+            # draw the line between location and destination
             line = folium.Polyline(location=(pointA, pointB), weight=2, color='white')
             m.add_child(line)
             # folium map modification
@@ -266,12 +246,12 @@ class ContactView(generic.FormView):
 
         m = m._repr_html_()
 
-
         context['distance'] = distance
         context['m_form'] = measurement_form
         context['map'] = m
         context['destination'] = destination
         return context
+
 
     def form_valid(self, form):
         # Create a new Contact object with the submitted form data
