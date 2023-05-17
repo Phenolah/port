2a4c9bbef990d706d4ac78702d13b1e55fb5ac6d
Phenolah <104977409+Phenolah@users.noreply.github.com>
2023-05-17 13:00:21 -0700

    Update views.py

--- personalportfolio/views.py
+++ personalportfolio/views.py
@@ -41,8 +41,10 @@ class HomeView(generic.TemplateView):
     def post(self, request, *args, **kwargs):
         print('HomeView.get_context_data() was called')
         contact_form = ContactForm(request.POST)
+        print(contact_form.errors)
         cv_form = CvFileForm(request.POST, request.FILES)
         if contact_form.is_valid():
+            print(contact_form.cleaned_data)
             # Create a new Contact object with the submitted form data
             contact = Contact(
                 name=contact_form.cleaned_data['name'],
@@ -50,12 +52,14 @@ class HomeView(generic.TemplateView):
                 subject=contact_form.cleaned_data['subject'],
                 message=contact_form.cleaned_data['message'],
             )
+            print(form.cleaned_data)
             contact.save()
             # Add a success message to the user's session
             messages.success(self.request,
                              "Success! Thank you for contacting me. I'll get back to you as soon as possible")
             # Redirect the user to the success page
-            return super().form_valid(contact_form)
+
+            return redirect("homepage")
         elif cv_form.is_valid():
             cv_upload = CvFileForm(request.FILES['resume'])
             cv_upload.save()
@@ -179,79 +183,6 @@ class ContactView(generic.FormView):
     template_name = 'contact.html'
     model = Contact
     form_class = ContactForm
-
-    def get_ip_address(request):
-        x_forwarded_fro = request.META.get('HTTP_X_FORWARDED_FOR')
-        if x_forwarded_fro:
-            ip = x_forwarded_fro.split(',')[0]
-        else:
-            ip = request.META.get('REMOTE_ADDR')
-        return ip
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        distance = None
-        destination = None
-        obj = get_object_or_404(Measurement, id=1)
-        measurement_form = MeasurementModelForm(self.request.POST or None)
-        geolocator = Nominatim(user_agent="measurements")
-
-        ip_ = get_ip_address(self.request)
-        print(ip_)
-        ip = '72.14.207.99'
-        country, city, lat, lon = get_geo(ip)
-        location = geolocator.geocode(city)
-        # location coordinates
-        l_lat = lat
-        l_lon = lon
-        pointA = (l_lat, l_lon)
-
-        # initial folium map
-        m = folium.Map(width=800, height=500, location=get_center_coordinates(l_lat, l_lon), zoom_start=100)
-        # location marker
-        folium.Marker([l_lat, l_lon], tooltip='clock here for more', popup=city['city'],
-                      icon=folium.Icon(color="pink")).add_to(m)
-
-        if measurement_form.is_valid():
-            instance = measurement_form.save(commit=False)
-            destination_ = measurement_form.cleaned_data.get('destination')
-            destination = geolocator.geocode(destination_)
-            print(destination)
-            # distance coordinates
-            d_lon = destination.longitude
-            d_lat = destination.latitude
-            # distance coordinates
-            d_lat = destination.latitude
-            d_lon = destination.longitude
-            pointB = (d_lat, d_lon)
-
-            # distance calculation
-            distance = round(geodesic(pointA, pointB).km, 2)
-            # initial folium map
-            m = folium.Map(width=800, height=500, location=get_center_coordinates(l_lat, l_lon),
-                           zoom_start=get_zoom(distance))
-            # location marker
-            folium.Marker([d_lat, d_lon], tooltip='clock here for more', popup=destination,
-                          icon=folium.Icon(color="pink")).add_to(m)
-            # location marker
-            folium.Marker([l_lat, l_lon], tooltip='clock here for more', popup=city['city'],
-                          icon=folium.Icon(color="red", icon='cloud')).add_to(m)
-
-            # draw the line between location and destination
-            line = folium.Polyline(location=(pointA, pointB), weight=2, color='white')
-            m.add_child(line)
-            # folium map modification
-            instance.location = "Nairobi"
-            instance.distance = 5000.0
-            instance.save()
-
-        m = m._repr_html_()
-
-        context['distance'] = distance
-        context['m_form'] = measurement_form
-        context['map'] = m
-        context['destination'] = destination
-        return context
 
 
     def form_valid(self, form):
